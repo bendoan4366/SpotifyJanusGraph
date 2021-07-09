ba3a7a4c6f465193658808892042c89a8e1d3868
bendoan4366 <ben.doan4366@gmail.com>
2021-07-08 22:30:41 -0400

    adding spotify audio extraction utils

--- python/utils/spotify_utils.py
+++ python/utils/spotify_utils.py
@@ -1,19 +1,47 @@
 import pandas as pd
 
-def get_user_playlists_dataframe(spotipy, user):
 
+def get_user_playlists_dataframe(spotipy, user):
     user_playlists = spotipy.user_playlists(user)
     playlist_dataframe = pd.DataFrame.from_dict(user_playlists['items'])
 
     return playlist_dataframe
 
 
-def get_playlist_tracks_dataframe(spotipy, playlist_id):
-
+def get_playlist_items(spotipy, playlist_id):
     playlist_items = spotipy.playlist_items(playlist_id)
-    print(type(playlist_items))
     playlist_tracks = [sub['track'] for sub in playlist_items['items']]
     playlist_items_dataframe = pd.DataFrame.from_dict(playlist_tracks)
 
     return playlist_items_dataframe
 
+
+def get_playlist_items_from_list(spotipy, user):
+    playlist_dataframe = get_user_playlists_dataframe(spotipy, user)
+    playlist_id_list = playlist_dataframe["id"].tolist()
+
+    playlist_item_ids_dataframe = pd.DataFrame(columns=["id", "name"])
+
+    for playlist_id in playlist_id_list:
+        playlist_items_dataframe = get_playlist_items(spotipy, playlist_id)
+        playlist_item_ids_dataframe = playlist_item_ids_dataframe.append(playlist_items_dataframe[["id", "name"]])
+
+    return playlist_item_ids_dataframe
+
+
+def get_track_audio_features(spotipy, track_id):
+    track_audio_features = spotipy.audio_features(track_id)
+
+    return track_audio_features
+
+
+def get_playlist_tracks_audio_features(spotipy, user):
+    playlist_items_dataframe = get_playlist_items_from_list(spotipy, user)
+
+    playlist_track_ids_list = playlist_items_dataframe["id"].tolist()
+    print(playlist_track_ids_list)
+    playlist_audio_features = get_track_audio_features(spotipy, playlist_track_ids_list)
+
+    playlist_audio_features_dataframe = pd.DataFrame.from_dict(playlist_audio_features)
+
+    return playlist_audio_features_dataframe
