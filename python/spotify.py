023fa0bff750304b71d2e74150377003517d2582
bendoan4366 <ben.doan4366@gmail.com>
2021-07-08 22:29:55 -0400

    adding audio feature extraction

--- python/spotify.py
+++ python/spotify.py
@@ -4,12 +4,13 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 import utils.spotify_utils
 
-#extract credentials from environment variabes and instantiate session
+# extract credentials from environment variabes and instantiate session
 auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 myPlaylists = utils.spotify_utils.get_user_playlists_dataframe(sp, "ben.doan4366")
-ids = myPlaylists.loc[:, ["id", "name"]]
+ids = myPlaylists["id"].tolist()
 
-id_dict = ids.to_dict()
-print(id_dict)
+myPlaylistItems = utils.spotify_utils.get_playlist_items_dataframe(sp, "51M1PfaricmfiaFbJPFggl")
+
+print(myPlaylistItems)
